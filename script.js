325e0b51f1c4688a87cb75ed7649b2dd17d27b37
abduvosidvaliyev <valiyevabduvosid6761@gmail.com>
2024-09-22 11:14:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,567 @@
+const data = [
+    {
+        img: "https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg",
+        title: "Nike Air Monarch IV",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "200",
+        company: "Nike",
+        color: "white",
+        category: "sneakers",
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/519MRhRKGFL._AC_UX575_.jpg",
+        title: "Nike Air Vapormax Plus",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "200",
+        company: "Nike",
+        color: "red",
+        category: "sneakers",
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/51+P9uAvb1L._AC_UY695_.jpg",
+        title: "Nike Waffle One Sneaker",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "200",
+        company: "Nike",
+        color: "green",
+        category: "sneakers",
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/71oEKkghg-L._AC_UX575_.jpg",
+        title: "Nike Running Shoe",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "200",
+        company: "Adidas",
+        color: "black",
+        category: "sneakers",
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/41M54ztS6IL._AC_SY625._SX._UX._SY._UY_.jpg",
+        title: "Flat Slip On Pumps",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "200",
+        company: "Vans",
+        color: "green",
+        category: "flats",
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/71zKuNICJAL._AC_UX625_.jpg",
+        title: "Knit Ballet Flat",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "50",
+        company: "Adidas",
+        color: "black",
+        category: "flats",
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/61V9APfz97L._AC_UY695_.jpg",
+        title: "Loafer Flats",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "50",
+        company: "Vans",
+        color: "white",
+        category: "flats",
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/71VaQ+V6XnL._AC_UY695_.jpg",
+        title: "Nike Zoom Freak",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "200",
+        company: "Nike",
+        color: "green",
+        category: "sneakers",
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/61-cBsLhJHL._AC_UY695_.jpg",
+        title: "Nike Men's Sneaker",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "200",
+        company: "Adidas",
+        color: "blue",
+        category: "sneakers",
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/81xXDjojYKS._AC_UX575_.jpg",
+        title: "PUMA BLACK-OCE",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "150",
+        company: "Puma",
+        color: "green",
+        category: "sneakers",
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/71E75yRwCDL._AC_UY575_.jpg",
+        title: "Pacer Future Sneaker",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "150",
+        company: "Puma",
+        color: "red",
+        category: "sneakers",
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/71jeoX0rMBL._AC_UX575_.jpg",
+        title: "Unisex-Adult Super",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "150",
+        company: "Puma",
+        color: "black",
+        category: "sneakers",
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/61TM6Q9dvxL._AC_UX575_.jpg",
+        title: "Roma Basic Sneaker",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "150",
+        company: "Puma",
+        color: "white",
+        category: "sneakers",
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/7128-af7joL._AC_UY575_.jpg",
+        title: "Pacer Future Doubleknit",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "150",
+        company: "Puma",
+        color: "black",
+        category: "sneakers",
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/81xXDjojYKS._AC_UX575_.jpg",
+        title: "Fusion Evo Golf Shoe",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "100",
+        company: "Puma",
+        color: "green",
+        category: "sneakers",
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/719gdz8lsTS._AC_UX575_.jpg",
+        title: "Rainbow Chex Skate",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "100",
+        company: "Vans",
+        color: "red",
+        category: "flats",
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/71gpFHJlnoL._AC_UX575_.jpg",
+        title: "Low-Top Trainers",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "100",
+        company: "Vans",
+        color: "white",
+        category: "sandals",
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/71pf7VFs9CL._AC_UX575_.jpg",
+        title: "Vans Unisex Low-Top",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "100",
+        company: "Vans",
+        color: "blue",
+        category: "sandals",
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/61N4GyWcHPL._AC_UY575_.jpg",
+        title: "Classic Bandana Sneakers",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "50",
+        company: "Nike",
+        color: "black",
+        category: "sandals",
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/61bncQ44yML._AC_UX695_.jpg",
+        title: "Chunky High Heel",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "50",
+        company: "Vans",
+        color: "black",
+        category: "heels",
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/71czu7WgGuL._AC_UY695_.jpg",
+        title: "Slip On Stiletto High Heel",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "100",
+        company: "puma",
+        color: "black",
+        category: "heels",
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/61men05KRxL._AC_UY625_.jpg",
+        title: "DREAM PAIRS Court Shoes",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "150",
+        company: "Nike",
+        color: "red",
+        category: "heels",
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/519MRhRKGFL._AC_UX575_.jpg",
+        title: "Nike Air Vapormax Plus",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "200",
+        company: "Nike",
+        color: "red",
+        category: "sneakers",
+    },
+
+    {
+        img: "https://m.media-amazon.com/images/I/51PGWTXgf-L._AC_UY625_.jpg",
+        title: "Low Mid Block Heels",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "200",
+        company: "Nike",
+        color: "black",
+        category: "heels",
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/616sA5XUKtL._AC_UY675_.jpg",
+        title: "Chunky High Heel",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "50",
+        company: "Adidas",
+        color: "black",
+        category: "heels",
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/71h5+MbEK7L._AC_UY625_.jpg",
+        title: "Amore Fashion Stilettos",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "150",
+        company: "Adidas",
+        color: "white",
+        category: "heels",
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/61uw5RDxKQL._AC_UY625_.jpg",
+        title: "Bridal Sandals Glitter",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "50",
+        company: "Adidas",
+        color: "black",
+        category: "heels",
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/71yhoZP0l6L._AC_UY695_.jpg",
+        title: "Wedding Prom Bridal",
+
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "50",
+        company: "Adidas",
+        color: "black",
+        category: "flats",
+    },
+];
+
+let container = document.querySelector(".card-container")
+let inp = document.querySelector(".inp")
+
+inp.addEventListener("input", qidiruv)
+
+function qidiruv() {
+    container.innerHTML = ""
+    data.filter(element => element.title.toLowerCase().includes(inp.value.toLowerCase()))
+        .forEach(item => {
+            let card = document.createElement("div")
+            card.innerHTML = `
+    <img src="${item.img}" alt="">
+    <div class="text">
+        <h3>${item.title}</h3>
+        <div class="star">
+            <div>
+                <i class="bi bi-star-fill goldd"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+            </div>
+            <h5>4</h5>
+        </div>
+        <div class="hart">
+            <div>
+                <s>${item.prevPrice}</s>
+                <h5>$${item.newPrice}</h5>
+            </div>
+            <i class="bi bi-bag-heart-fill"></i>
+        </div>
+            </div>`
+            card.classList.add("card")
+            container.appendChild(card)
+        });
+}
+
+qidiruv()
+
+let btn = document.querySelector(".btn")
+
+btn.addEventListener("click", () => {
+
+})
+
+let categoryButton = new Set(data.map(item => item.category))
+let buttons = document.querySelector(".buttons")
+
+let search = "all"
+
+function Qidiruv() {
+    container.innerHTML = ""
+    data.filter(element => search === "all" ? element : element.category.includes(search))
+        .forEach(item => {
+            let card = document.createElement("div")
+            card.innerHTML = `
+    <img src="${item.img}" alt="">
+    <div class="text">
+        <h3>${item.title}</h3>
+        <div class="star">
+            <div>
+                <i class="bi bi-star-fill goldd"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+            </div>
+            <h5>4</h5>
+        </div>
+        <div class="hart">
+            <div>
+                <s>${item.prevPrice}</s>
+                <h5>$${item.newPrice}</h5>
+            </div>
+            <i class="bi bi-bag-heart-fill"></i>
+        </div>
+            </div>`
+            card.classList.add("card")
+            container.appendChild(card)
+        });
+}
+
+categoryButton.forEach(element => {
+    let button = document.createElement("button")
+    button.innerHTML = element
+    buttons.appendChild(button)
+    button.addEventListener("click", () => {
+        search = button.innerHTML
+        Qidiruv()
+    })
+});
+
+let btn1 = document.querySelector(".btn")
+btn.addEventListener("click", () => {
+    search = "all"
+    Qidiruv()
+})
+
+let all = document.querySelectorAll(".all")
+
+all.forEach(element => {
+    element.addEventListener("click", () => {
+        search = "all"
+        Qidiruv()
+    })
+});
+
+let categoryy = document.querySelector(".categoriyy")
+
+let al = document.querySelectorAll(".al")
+
+al.forEach(element => {
+    element.addEventListener("click", () => {
+        search = "all"
+        Qidiruv()
+    })
+});
+
+categoryButton.forEach(item => {
+    let label = document.createElement("label")
+    let inp = document.createElement("input")
+    label.innerHTML = `${item}`
+    label.for = "all"
+    inp.type = "radio"
+    inp.name = "all"
+
+    label.appendChild(inp)
+    categoryy.appendChild(label)
+
+    label.addEventListener("click", () => {
+        search = item
+        Qidiruv()
+    })
+})
+
+let price = document.querySelector(".price")
+
+let newprice = new Set(data.map(item => item.newPrice))
+
+let priceSearch = ""
+
+function priceFilter() {
+    container.innerHTML = ""
+    data.filter(item => item.newPrice == priceSearch ? item : "" )
+        .forEach(item => {
+            let card = document.createElement("div")
+            card.innerHTML = `
+    <img src="${item.img}" alt="">
+    <div class="text">
+        <h3>${item.title}</h3>
+        <div class="star">
+            <div>
+                <i class="bi bi-star-fill goldd"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+            </div>
+            <h5>4</h5>
+        </div>
+        <div class="hart">
+            <div>
+                <s>${item.prevPrice}</s>
+                <h5>$${item.newPrice}</h5>
+            </div>
+            <i class="bi bi-bag-heart-fill"></i>
+        </div>
+            </div>`
+            card.classList.add("card")
+            container.appendChild(card)
+        });
+}
+
+newprice.forEach(item => {
+    let label = document.createElement("label")
+    let inp = document.createElement("input")
+    label.innerHTML = `0 - ${item}$`
+    label.for = "ai"
+    inp.type = "radio"
+    inp.name = "ai"
+
+    label.appendChild(inp)
+    price.appendChild(label)
+
+    label.addEventListener("click", () => {
+        priceSearch = item
+        console.log(priceSearch);
+        
+        priceFilter()
+    })
+})
+
+let newColor = new Set(data.map(item => item.color))
+
+let searchColor = ""
+
+let colors = document.querySelector(".colors")
+
+function filterColors() {
+    container.innerHTML = ""
+    data.filter(item => item.color === searchColor ? item : "")
+        .forEach(item => {
+            let card = document.createElement("div")
+            card.innerHTML = `
+    <img src="${item.img}" alt="">
+    <div class="text">
+        <h3>${item.title}</h3>
+        <div class="star">
+            <div>
+                <i class="bi bi-star-fill goldd"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+            </div>
+            <h5>4</h5>
+        </div>
+        <div class="hart">
+            <div>
+                <s>${item.prevPrice}</s>
+                <h5>$${item.newPrice}</h5>
+            </div>
+            <i class="bi bi-bag-heart-fill"></i>
+        </div>
+            </div>`
+            card.classList.add("card")
+            container.appendChild(card)
+        });
+}
+
+newColor.forEach(element => {
+    let label = document.createElement("label")
+    let inp = document.createElement("input")
+    label.innerHTML = element
+    label.classList.add("labelColor")
+    label.for = "cl"
+    inp.type = "radio"
+    inp.name = "cl"
+
+    label.appendChild(inp)
+    colors.appendChild(label)
+
+    label.addEventListener("click", () => {
+        searchColor = element
+        console.log(searchColor);
+        
+        filterColors()
+    })
+});
